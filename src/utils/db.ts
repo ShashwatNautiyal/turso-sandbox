5f43a298c9bbba68b8ab966270039ff9afbd2646
Shashwat Nautiyal <shashwatnautiyal2015@gmail.com>
2023-03-23 22:25:59 +0530

    upgrade libsql version

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,15 +1,15 @@
 import { createClient, Client } from "@libsql/client";
-import DatabaseConstructor, { Database } from "better-sqlite3";
 
 class TursoDB {
 	private db: any;
 	private url: string;
 	constructor(url: string) {
 		this.url = url;
-		if (!this.url.includes("@") || !this.url.includes("-")) {
+		if (!this.url.includes("turso.io")) {
 			throw new Error("You must provide a valid http URL");
 		} else {
-			this.db = createClient({ url });
+			this.db = createClient({ url: this.url });
+			console.log(this.db);
 		}
 	}
 
@@ -22,7 +22,7 @@ class TursoDB {
 	}
 
 	getName() {
-		if (!this.url.includes("@") || !this.url.includes("-")) return "Local DB";
+		if (!this.url.includes("@")) return "Unknown DB";
 		return this.url.split("@").pop()?.split("-").shift() ?? "";
 	}
 }
